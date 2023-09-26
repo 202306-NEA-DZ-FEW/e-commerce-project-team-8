b2db2d6d5195ee437de09cb5764a0c3342679126
benarba mohammed <tewfikhotspur@gmail.com>
2023-09-26 16:04:18 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,5 @@
 import Image from "next/image"
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-      hello world
-    </main>
-  )
+  return <h1>heloo </h1>
 }
