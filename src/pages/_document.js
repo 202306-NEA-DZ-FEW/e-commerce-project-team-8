685d39457c9b75d8efd005156edd49d39b5ec3fc
Bolphunga <aymendj@yahoo.com>
2023-09-28 15:12:40 +0200

    feat: add a footer
    #5

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer/Footer"
 import Navbar from "@/components/Navbar/Navbar"
 import { Html, Head, Main, NextScript } from "next/document"
 
@@ -9,6 +10,7 @@ export default function Document() {
         <Navbar />
         <Main />
         <NextScript />
+        <Footer />
       </body>
     </Html>
   )
